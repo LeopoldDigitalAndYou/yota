1000af4125be10b6691c960114c4d4744605fc86
LeopoldDigitalAndYou <leo@garage56.fr>
2016-12-08 17:14:09 +0100

    add Foundation

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,11 @@
+import 'script!jquery';
+import 'script!foundation-sites/dist/js/foundation.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+$(document).ready(function ($) {
+	$(document).foundation();
+});
 
 ReactDOM.render(
 		<h1>Hello, world!</h1>,
