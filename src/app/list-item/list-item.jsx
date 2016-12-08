1f2e7b835d56da1a21a799d0e2083a4de056ac2a
LeopoldDigitalAndYou <leo@garage56.fr>
2016-12-08 18:24:24 +0100

    how does state work

--- src/app/list-item/list-item.jsx
+++ src/app/list-item/list-item.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 class ListItem extends React.Component {
 	constructor(props) {
 		super(props);
-		this.title = props.title || "Do something new…";
-		this.done = !!props.done;
+		this.state = {
+			title: props.title || "Do something new…",
+			done: !!props.done
+		};
 	}
+
 	render() {
-		return <div className={'todo-list-item ' + (this.props.done ? 'status-done' : 'status-todo')}>
-			<h3>{this.title}</h3>
+		return <div className={'todo-list-item ' + (this.state.done ? 'status-done' : 'status-todo')}>
+			<h3>{this.state.title}</h3>
 		</div>;
 	}
 }
